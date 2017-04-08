e82d0e5649b8ee48e05e53fa6bd9790e9cc5749d
mjuneja89 <manalsujuneja@hotmail.com>
2017-04-08 17:32:02 +0530

    init

---
+++ app/controllers/carpets_controller.rb
@@ -0,0 +1,12 @@
+class CarpetsController < ApplicationController
+    
+    def handtufted
+    end
+
+    def handloomweave
+    end
+
+    def handloomdurries
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
   root 'home#home'
+
+  get '/handtufted' => 'carpets#handtufted'
+
+  get '/handloomweave' => 'carpets#handloomweave'
+
+  get '/handloomdurries' => 'carpets#handloomdurries'
+
+
 end
