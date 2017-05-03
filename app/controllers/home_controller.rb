7e8e34ecadc1bb60a61b2ce421774fa845fa93b3
mjuneja89 <manalsujuneja@hotmail.com>
2017-05-03 18:14:39 +0530

    init

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 	def home
-		@handtufted = Carpet.where(:category => 'hand tufted').limit(4)
-		@handloomweave = Carpet.where(:category => 'handloom weave').limit(4)
-		@handknotted = Carpet.where(:category => 'hand knotted').limit(4)
-		@handloomdurries = Carpet.where(:category => 'handloom durries').limit(4)
+		@handtufted = Carpet.where(:category => 'hand tufted').order(:priority_id).limit(4)
+		@handloomweave = Carpet.where(:category => 'handloom weave').order(:priority_id).limit(4)
+		@handknotted = Carpet.where(:category => 'hand knotted').order(:priority_id).limit(4)
+		@handloomdurries = Carpet.where(:category => 'handloom durries').order(:priority_id).limit(4)
 	end
 end
