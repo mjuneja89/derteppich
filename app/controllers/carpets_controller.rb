7a6821be7fe6ab74902fe6cf949b16eee24b9371
mjuneja89 <manalsujuneja@hotmail.com>
2017-05-03 18:03:19 +0530

    init

--- app/controllers/carpets_controller.rb
+++ app/controllers/carpets_controller.rb
@@ -18,16 +18,20 @@ class CarpetsController < ApplicationController
     
 
     def new
+      if current_user  
        @carpet = Carpet.new
+      end 
     end
 
     def create
+      if current_user  
        @carpet = Carpet.new(carpet_params)	
        if @carpet.save
        	redirect_to adminview_path
        else
        	render 'new'
        end
+      end 
     end
 
     def show
@@ -35,51 +39,67 @@ class CarpetsController < ApplicationController
     end
 
     def edit
+       if current_user 
     	@carpet = Carpet.find(params[:id])
+       end
     end
 
     def update
+       if current_user 
     	@carpet = Carpet.find(params[:id])
     	if @carpet.update_attributes(carpet_params)
     		redirect_to adminview_path
     	else
     		render 'edit'
     	end
+       end 
     end
 
     def index
+       if current_user 
         @handtufted = Carpet.where(:category => 'hand tufted').order(:priority_id)
         @handloomweave = Carpet.where(:category => 'handloom weave').order(:priority_id)
         @handknotted = Carpet.where(:category => 'hand knotted').order(:priority_id)
         @handloomdurries = Carpet.where(:category => 'handloom durries').order(:priority_id)
+       end 
     end
 
     def destroy
+       if current_user 
         @carpet = Carpet.find(params[:id])
         Carpet.destroy(@carpet)
         redirect_to carpets_path
+       end 
     end
     
     def editpriority
+       if current_user 
         @carpet = Carpet.find(params[:carpet_id])
+       end 
     end
 
     def updatepriority
+       if current_user 
         @carpet = Carpet.find(params[:carpet_id])
-        if @carpet.update_attributes(category_params)
+        if @carpet.update_attributes(carpet_params)
             redirect_to adminview_path
         end 
+       end 
     end
 
     def editcategory
+       if current_user 
         @carpet = Carpet.find(params[:carpet_id])
+       end 
     end
 
     def updatecategory
+       if current_user 
         @carpet = Carpet.find(params[:carpet_id])
-        if @carpet.update_attributes(category_params)
+        if @carpet.update_attributes(carpet_params)
             redirect_to adminview_path
         end 
+       end 
     end
 
     private
